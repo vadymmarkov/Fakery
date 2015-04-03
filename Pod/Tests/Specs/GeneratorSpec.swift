a1415c11b249c868df200f28413d345e9b3ab823
Vadim Markov <impressionwave@gmail.com>
2015-04-03 15:27:48 +0200

    Implement parse fucntionality

--- Pod/Tests/Specs/GeneratorSpec.swift
+++ Pod/Tests/Specs/GeneratorSpec.swift
@@ -38,6 +38,29 @@ class GeneratorSpec: QuickSpec
                     expect(generator.dataProvider).notTo(beNil())
                 }
             }
+
+            describe("#numerify") {
+                it("replaces # with random numbers") {
+                    let numerified = generator.numerify("12####")
+                    expect(numerified.toInt()).notTo(beNil())
+                    expect(find(numerified, "#")).to(beNil())
+                }
+            }
+
+            describe("letterify") {
+                it("replaces ? with random letters") {
+                    let letterified = generator.letterify("This is awes?me")
+                    expect(find(letterified, "?")).to(beNil())
+                }
+            }
+
+            describe("bothify") {
+                it("replaces # with random numbers and ? with random letters") {
+                    let bothified = generator.bothify("#th of ?pril")
+                    expect(find(bothified, "#")).to(beNil())
+                    expect(find(bothified, "?")).to(beNil())
+                }
+            }
         }
     }
 }
