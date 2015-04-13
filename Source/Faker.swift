8f6c1ed6720ed4e9194a04851cde45b80d5fa130
Vadym Markov <impressionwave@gmail.com>
2015-04-13 23:56:08 +0200

    Add generators to faker

--- Source/Faker.swift
+++ Source/Faker.swift
@@ -9,6 +9,14 @@ public class Faker {
     }
 
     public let address: Address
+    public let app: App
+    public let business: Business
+    public let commerce: Commerce
+    public let internet: Internet
+    public let lorem: Lorem
+    public let name: Name
+    public let phoneNumber: PhoneNumber
+    public let team: Team
 
     let parser: Parser
 
@@ -17,5 +25,13 @@ public class Faker {
         parser = Parser(locale: self.locale)
 
         address = Address(parser: parser)
+        app = App(parser: parser)
+        business = Business(parser: parser)
+        commerce = Commerce(parser: parser)
+        internet = Internet(parser: parser)
+        lorem = Lorem(parser: parser)
+        name = Name(parser: parser)
+        phoneNumber = PhoneNumber(parser: parser)
+        team = Team(parser: parser)
     }
 }
