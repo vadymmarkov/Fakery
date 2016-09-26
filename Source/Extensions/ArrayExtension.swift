adbd31b9c8a460bb18c428ed2b070f19dd069cf4
Vadym Markov <markov.vadym@gmail.com>
2016-09-26 22:45:58 +0200

    Refactor array extension

--- Source/Extensions/ArrayExtension.swift
+++ Source/Extensions/ArrayExtension.swift
@@ -3,20 +3,18 @@ import Foundation
 extension Array {
 
   func at(index: Int?) -> Element? {
-    if let index = index where index >= 0 && index < endIndex {
-      return self[index]
-    } else {
+    guard let index = index where index >= 0 && index < endIndex else {
       return nil
     }
+
+    return self[index]
   }
 
   func random() -> Element? {
-    var object: Element?
-
-    if count > 0 {
-      object = self[Int(arc4random_uniform(UInt32(count)))]
+    guard count > 0 else {
+      return nil
     }
 
-    return object
+    return self[Int(arc4random_uniform(UInt32(count)))]
   }
 }
