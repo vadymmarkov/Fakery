48d550eb9ef003b491005613f9c305faf1f6a74a
Vadym Markov <markov.vadym@gmail.com>
2017-10-17 00:01:53 +0200

    Update Package.swift to Swift 4

--- Package.swift
+++ Package.swift
@@ -1,8 +1,10 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "Fakery",
-    exclude: [
-        "Tests"
+    targets: [
+      .target(name: "Fakery")
     ]
 )
