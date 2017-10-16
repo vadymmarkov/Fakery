c771734998628be37b37974726eb75a7b75d8d72
Vadym Markov <markov.vadym@gmail.com>
2017-10-16 21:57:07 +0200

    Fallback to default locale

--- Source/Config.swift
+++ Source/Config.swift
@@ -1,6 +1,6 @@
 public struct Config {
   public static let defaultLocale: String = "en"
+  public static let pathExtension: String = "json"
   public static var dirPath: String = "Resources/Locales"
   public static var dirFrameworkPath: String = ""
-  public static let pathExtension: String = "json"
 }
