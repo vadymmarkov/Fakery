ecc99a96d0afca612667793171d81835a190f407
Kevin Seidel <kevin@zahlz.com>
2017-10-19 11:44:56 +0200

    Add Product to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "Fakery",
+    products: [
+      .library(name: "Fakery", targets: ["Fakery"])
+    ],
     targets: [
       .target(name: "Fakery")
     ]
