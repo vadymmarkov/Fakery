bb55e7c9857e26704d15e930a1af25fe3c70ffff
Emrah Gunduz <emrah.gunduz@markakod.com>
2017-11-10 20:43:11 +0300

    Random coordinate in radius for given point
    Coordinate method to generate points inside a circle (radius as meters) for given latitude and longitude values.

--- Sources/Fakery/Generators/Address.swift
+++ Sources/Fakery/Generators/Address.swift
@@ -1,5 +1,10 @@
 import Foundation
 
+public struct FakeryCoordinates {
+  public var latitude:  Double
+  public var longitude: Double
+}
+
 public final class Address: Generator {
   public func city() -> String {
     return generate("address.city")
@@ -77,5 +82,31 @@ public final class Address: Generator {
 
   public func longitude() -> Double {
     return drand48() * 360.0 - 180.0
+  }
+  
+  public func coordinate(latitude: Double, longitude: Double, inRadius radius: Double = 1000) -> FakeryCoordinates {
+    let y0              = latitude
+    let x0              = longitude
+    
+    // Convert meters to degrees for raduis
+    let radiusInDegrees = radius / 111300.0
+    
+    // Random distance in circle
+    let u               = Double(arc4random()) / 0xFFFFFFFF
+    let v               = Double(arc4random()) / 0xFFFFFFFF
+    let w               = radiusInDegrees * sqrt(u)
+    let t               = 2 * .pi * v
+    let x               = w * cos(t)
+    let y               = w * sin(t)
+    
+    // Adjust longitude (x) to adjust for east-west shrinking in distance
+    let latRadians      = y0 * .pi / 180
+    let newx            = x / cos(latRadians)
+    
+    // Set found random point
+    let foundLatitude   = y + y0
+    let foundLongitude  = newx + x0
+    
+    return FakeryCoordinates.init(latitude: foundLatitude, longitude: foundLongitude)
   }
 }
