4f79f96fac810d8d01a8341974722f6592a91999
Brian Strobach <brian@appsaurus.io>
2018-05-22 11:20:36 -0500

    -POC for true Swift Package Manager compatibility. Replacing .json file with json string in swift source.

--- Sources/Fakery/Data/Provider.swift
+++ Sources/Fakery/Data/Provider.swift
@@ -11,33 +11,10 @@ public final class Provider {
     if let translationData = translations[locale] {
       translation = translationData
     } else {
-      let bundle = Bundle(for: Provider.self)
-      var path = bundle.path(forResource: locale,
-                             ofType: Config.pathExtension,
-                             inDirectory: Config.dirPath)
-
-      if path == nil {
-        path = bundle.path(forResource: locale,
-                           ofType: Config.pathExtension,
-                           inDirectory: Config.dirFrameworkPath)
-      }
-
-      if let resourcePath = Bundle(for: Provider.self).resourcePath {
-        let bundlePath = resourcePath + "/Faker.bundle"
-
-        if let bundle = Bundle(path: bundlePath) {
-          path = bundle.path(forResource: locale, ofType: Config.pathExtension)
-        }
-      }
-
-      if let path = path {
-        let fileURL = URL(fileURLWithPath: path)
-
-        if let data = try? Data(contentsOf: fileURL) {
-          translation = data
-          translations[locale] = data
-        }
-      }
+			if let data = Locale.en.data(using: .utf8){
+				translation = data
+				translations[locale] = data
+			}
     }
 
     return translation
