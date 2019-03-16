0777445637ba8ac5c0893a3b519938b85d301c01
Gustavo Campos <guseducampos@gmail.com>
2019-03-16 17:23:25 -0600

    allow run tests with swift package manager.

--- Package.swift
+++ Package.swift
@@ -5,9 +5,16 @@ import PackageDescription
 let package = Package(
     name: "Fakery",
     products: [
-      .library(name: "Fakery", targets: ["Fakery"])
+        .library(name: "Fakery", targets: ["Fakery"])
+    ],
+    dependencies: [
+    // Test dependencies
+    .package(url: "https://github.com/Quick/Quick.git", from: "2.0.0"),
+    .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
+
     ],
     targets: [
-      .target(name: "Fakery")
+      .target(name: "Fakery"),
+      .testTarget(name: "FakeryTests", dependencies: ["Fakery","Quick", "Nimble"], path: "Tests/Fakery")
     ]
 )
