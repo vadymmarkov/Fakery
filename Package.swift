dac28a1ef599f8e3ce92f5449de60a3ff78777a1
Jose Gil <jose.gil@tollaga.com>
2021-02-05 23:06:24 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
     // Test dependencies
     .package(url: "https://github.com/Quick/Quick.git", from: "2.0.0"),
-    .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
+    .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0"),
 
     ],
     targets: [
