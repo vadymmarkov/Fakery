0a7654ed2c9b0acf4aa9c39b6f4fbacfe790486d
Vadym Markov <markov.vadym@gmail.com>
2021-02-23 17:31:26 +0100

    Update Quick and Nimble

--- Package.swift
+++ Package.swift
@@ -5,16 +5,15 @@ import PackageDescription
 let package = Package(
     name: "Fakery",
     products: [
-        .library(name: "Fakery", targets: ["Fakery"])
+      .library(name: "Fakery", targets: ["Fakery"])
     ],
     dependencies: [
-    // Test dependencies
-    .package(url: "https://github.com/Quick/Quick.git", from: "3.1.0"),
-    .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0"),
-
+      // Test dependencies
+      .package(url: "https://github.com/Quick/Quick.git", from: "3.1.2"),
+      .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0"),
     ],
     targets: [
       .target(name: "Fakery", resources: [.process("Resources")]),
-      .testTarget(name: "FakeryTests", dependencies: ["Fakery","Quick", "Nimble"], path: "Tests/Fakery")
+      .testTarget(name: "FakeryTests", dependencies: ["Fakery", "Quick", "Nimble"], path: "Tests/Fakery")
     ]
 )
